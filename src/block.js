9a5feab0327a2da3edc2ac52999fd7c118e56ca8
Rohitash28 <rohitash_28100@yahoo.com>
2020-05-25 14:13:05 +0530

    fourth submit

--- src/block.js
+++ src/block.js
@@ -35,23 +35,23 @@ class Block {
    *  Note: to access the class values inside a Promise code you need to create an auxiliary value `let self = this;`
    */
   validate() {
-    let curHash = self.curHash;
-      self.curHash = null;
+    let curHash = self.hash;
+      self.hash = null;
 
     return new Promise((resolve) => {
       // Save in auxiliary variable the current block hash
       
       // Recalculate the hash of the Block
-      const recalculatedHash = SHA256(JSON.stringify(self)).toString();
+      const newHash = SHA256(JSON.stringify(self)).toString();
       // Comparing if the hashes changed
       // Returning the Block is not valid
       // Returning the Block is valid
-       self.curHash= curHash;
-      if(curHash === recalculatedHash){
-           reslove(true);
+       self.hash= curHash;
+      if(curHash === newHash){
+           resolve(true);
              }
       else{
-         reslove(false);
+         resolve(false);
          }
 
     });
